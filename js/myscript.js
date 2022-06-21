d01ff615e2088033631c33fa3e654fbe566400b7
ErJump <fedelijunior@gmail.com>
2022-06-21 13:14:01 +0200

    Ex. Complete

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+const shoppingList = ['cioccolato', 'gelato', 'pane', 'latte', 'biscotti', 'formaggio'];
+
+let i = 0;
+
+while (i < shoppingList.length){
+    const shoppingElement = document.createElement('li');
+    shoppingElement.innerHTML = shoppingList[i];
+    document.getElementById('shopping-list').append(shoppingElement);
+    i++;
+}
